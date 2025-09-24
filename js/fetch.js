09e3eab36bb479e04ecb396ba48760b2f3a3b8d0
hassanhasu18-bob <hassan.hasu.18@gmail.com>
2025-09-24 15:20:14 +0200

    test

--- js/fetch.js
+++ js/fetch.js
@@ -2,20 +2,47 @@
 
 // En funktion som henter produkter fra en server
 export async function fetchProducts() {
+<<<<<<< Updated upstream
     try {
         // Forsøger at hente data fra API'et (serverens URL)
         const response = await fetch('https://glamping-rqu9j.ondigitalocean.app/reviews/');
+=======
+  try {
+    // Forsøger at hente data fra API'et (serverens URL)
+    const response = await fetch("https://glamping-rqu9j.ondigitalocean.app/");
+>>>>>>> Stashed changes
 
-        // Konverterer svaret fra API'et til JSON-format
-        const data = await response.json();
+    // Konverterer svaret fra API'et til JSON-format
+    const data = await response.json();
 
-        return data.data;
+    return data.data;
+  } catch (error) {
+    // Hvis der sker en fejl, så kommer fejlen i konsollen
+    console.error("Error fetching or parsing data:", error);
 
-    } catch (error) {
-        // Hvis der sker en fejl, så kommer fejlen i konsollen
-        console.error('Error fetching or parsing data:', error);
+    // Returnerer en tom liste, så programmet ikke går ned
+    return [];
+  }
+}
 
-        // Returnerer en tom liste, så programmet ikke går ned
-        return [];
-    }
-}   
+//glamping-rqu9j.ondigitalocean.app/stays/
+
+export async function fetchStays() {
+  try {
+    // Forsøger at hente data fra API'et (serverens URL)
+    const response = await fetch(
+      "https://glamping-rqu9j.ondigitalocean.app/stays/"
+    );
+
+    // Konverterer svaret fra API'et til JSON-format
+    const data = await response.json();
+
+    return data.data;
+  } catch (error) {
+    // Hvis der sker en fejl, så kommer fejlen i konsollen
+    console.error("Error fetching or parsing data:", error);
+
+    // Returnerer en tom liste, så programmet ikke går ned
+    return [];
+  }
+}
